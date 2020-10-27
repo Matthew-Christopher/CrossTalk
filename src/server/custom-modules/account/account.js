abd7ba0cfaf179fc0fe03ef224fe67e07eaa1b15
Matthew Christopher <matthewlukechristopher@gmail.com>
2020-10-27 17:52:52 +0000

    Update login page design

--- src/server/custom-modules/account/account.js
+++ src/server/custom-modules/account/account.js
@@ -3,5 +3,5 @@ const log = require('../logging');
 module.exports.LogIn = (email, password) => {
   log.info("Login request received.");
   log.info(`Email: ${email}`);
-  log.info(`Password: ${password}`);
+  log.info(`Password Hash: ${password}`);
 };
