db2e47a36885877c690ce6e3ba94de347fe59967
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-01-02 15:34:40 +0000

    Add settings nav

--- src/client/js/options-buttons.js
+++ src/client/js/options-buttons.js
@@ -1,12 +1,26 @@
 $(window).on("load", () => {
   $('#profile-options-button').click(() => {
-    ToggleVisiblity('#profile-options-container');
+    ToggleVisiblity('#profile-options-nav-container');
+  });
+
+  $('#options-button').click(() => {
+    ToggleVisiblity('#options-nav-container');
   });
 });
 
 function ToggleVisiblity (name) {
+  // If I'm visible, hide me.
+  // If I need to be shown, show me, and if the other nav item is visible then hide it too.
+
   let currentVisibility = $(name).css('visibility');
-  console.log(currentVisibility);
-  console.log((currentVisibility == 'visible') ? 'hidden' : 'visible');
-  $(name).css('visibility', (currentVisibility == 'visible') ? 'hidden' : 'visible'); // Toggle the visibility of the container.
+
+  if (($(name).css('visibility') == 'visible')) {
+    $(name).css('visibility', 'hidden');
+  } else {
+    if ($((name == '#profile-options-nav-container') ? '#options-nav-container' : '#profile-options-nav-container').css('visibility', 'visible')) {
+      $((name == '#profile-options-nav-container') ? '#options-nav-container' : '#profile-options-nav-container').css('visibility', 'hidden');
+    }
+    
+    $(name).css('visibility', 'visible');
+  }
 }
