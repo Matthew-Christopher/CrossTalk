b6c8ceb2d5b380c2593e90702a19e6b5d6e3f294
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-01-23 12:29:28 +0000

    Improve script to get completion percentage

--- GetCompletedTaskPercentage.sql
+++ GetCompletedTaskPercentage.sql
@@ -1,1 +1,1 @@
-SELECT (SELECT COUNT(*) FROM Message WHERE GroupID = "449dc213-567d-11eb-b511-7085c2833bdf" AND MessageString LIKE "✓%")/(SELECT COUNT(*) FROM Message WHERE GroupID = "449dc213-567d-11eb-b511-7085c2833bdf") * 100 AS CompletedPercentage;
+SELECT CONCAT(ROUND((SELECT COUNT(*) FROM Message WHERE GroupID = "449dc213-567d-11eb-b511-7085c2833bdf" AND MessageString LIKE "✓%")/(SELECT COUNT(*) FROM Message WHERE GroupID = "449dc213-567d-11eb-b511-7085c2833bdf") * 100, 1), '%') AS CompletedPercentage;
