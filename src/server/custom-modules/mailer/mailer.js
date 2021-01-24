ebf31feed348b4f500494d64799ec1dca89f69af
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-01-24 12:53:08 +0000

    Change disclaimer text in emails to bold

--- src/server/custom-modules/mailer/mailer.js
+++ src/server/custom-modules/mailer/mailer.js
@@ -15,7 +15,7 @@ module.exports.SendVerification = (email, verificationKey) => {
     from: process.env.NM_USER,
     to: email,
     subject: 'Verify Your Account',
-    html: '<h1>Welcome to Crosstalk</h1><p>Please click the link below to verify your account.</p><a href=\"localhost/verify?verificationKey=' + verificationKey + '\">localhost/verify?verificationKey=' + verificationKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><p>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</p>'
+    html: '<h1>Welcome to Crosstalk</h1><p>Please click the link below to verify your account.</p><a href=\"localhost/verify?verificationKey=' + verificationKey + '\">localhost/verify?verificationKey=' + verificationKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -28,7 +28,7 @@ module.exports.SendRecovery = (email, recoveryKey) => {
     from: process.env.NM_USER,
     to: email,
     subject: 'Recover Your Password',
-    html: '<h1>Crosstalk</h1><p>Please click the link below to recover your account by resetting your password.</p><a href=\"localhost/account/change-password?recoveryKey=' + recoveryKey + '\">localhost/account/change-password?recoveryKey=' + recoveryKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><p>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</p>'
+    html: '<h1>Crosstalk</h1><p>Please click the link below to recover your account by resetting your password.</p><a href=\"localhost/account/change-password?recoveryKey=' + recoveryKey + '\">localhost/account/change-password?recoveryKey=' + recoveryKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
   }
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -41,7 +41,7 @@ module.exports.SendChangeNotification = (displayName, email) => {
     from: process.env.NM_USER,
     to: email,
     subject: 'Notification of Password Change',
-    html: '<h1>Crosstalk</h1><p>Dear ' + displayName + ',</p><p>This automated email is to let you know that the password associated with your account was just changed. If this was you, you do not need to take any further action.</p><p>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</p>'
+    html: '<h1>Crosstalk</h1><p>Dear ' + displayName + ',</p><p>This automated email is to let you know that the password associated with your account was just changed. If this was you, you do not need to take any further action.</p><b>IMPORTANT NOTE: This project is part of my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
   }
 
   transporter.sendMail(mailOptions, (error, info) => {
