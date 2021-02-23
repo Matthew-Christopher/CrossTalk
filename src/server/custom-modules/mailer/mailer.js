a620e15faba89b7a796e7c47a9d43cec4f0c5a1b
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-02-23 16:48:45 +0000

    Update email link methods

--- src/server/custom-modules/mailer/mailer.js
+++ src/server/custom-modules/mailer/mailer.js
@@ -18,7 +18,7 @@ module.exports.SendVerification = (email, verificationKey) => {
     from: process.env.NM_USER,
     to: email,
     subject: 'Verify Your Account',
-    html: '<h1>Welcome to Crosstalk</h1><p>Please click the link below to verify your account.</p><a href=\"localhost/verify?verificationKey=' + verificationKey + '\">localhost/verify?verificationKey=' + verificationKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
+    html: '<h1>Welcome to Crosstalk</h1><p>Please click the link below to verify your account.</p><a href="' + process.env.EXTERNAL_IP_ADDRESS + '/verify?verificationKey=' + verificationKey + '">' + process.env.EXTERNAL_IP_ADDRESS + '/verify?verificationKey=' + verificationKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -31,7 +31,7 @@ module.exports.SendRecovery = (email, recoveryKey) => {
     from: process.env.NM_USER,
     to: email,
     subject: 'Recover Your Account',
-    html: '<h1>Crosstalk</h1><p>Please click the link below to recover your account by resetting your password.</p><a href=\"localhost/account/change-password?recoveryKey=' + recoveryKey + '\">localhost/account/change-password?recoveryKey=' + recoveryKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
+    html: '<h1>Crosstalk</h1><p>Please click the link below to recover your account by resetting your password.</p><a href="' + process.env.EXTERNAL_IP_ADDRESS + '/account/change-password?recoveryKey=' + recoveryKey + '">' + process.env.EXTERNAL_IP_ADDRESS + '/account/change-password?recoveryKey=' + recoveryKey + '</a><p><i>If you didn\'t request this, you can just ignore the email.</i></p><b>IMPORTANT NOTE: This project is my Computer Science A Level NEA. Please do not mistake this for an actual commericial service or product. You should not create or use an account if you have stumbled upon this website without being permission to use or test it. Thank you.</b>'
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
