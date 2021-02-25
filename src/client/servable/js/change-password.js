04a6f3bb849703ab28a086e8ffa3a5e0e507bde6
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-02-25 22:07:39 +0000

    Update documentation

--- src/client/servable/js/change-password.js
+++ src/client/servable/js/change-password.js
@@ -20,8 +20,14 @@ $(window).on('load', () => {
         if (JSONData.outcome == 'mismatch') {
           // Password did not match its confirmation field.
           $('#result').text('Passwords did not match.');
+
+          $('input[name="new-password"]').val('').focus();
+          $('input[name="confirm-new-password"]').removeClass('non-empty').val('');
         } else if (JSONData.outcome == 'password') {
           $('#result').text('Password does not meet the security requirements. It needs to be at least 8 characters long and contain an upper case letter, digit and a symbol. It must also not contain more than 2 repeated characters next to each other, like in "passwooord".');
+
+          $('input[name="new-password"]').val('').focus();
+          $('input[name="confirm-new-password"]').removeClass('non-empty').val('');
         } else if (JSONData.outcome == 'invalid') {
           // The recovery key was not valid.
           $('#result').text('Invalid recovery key; it may have expired. Redirecting...');
