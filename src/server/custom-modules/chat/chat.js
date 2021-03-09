3f495d77d31b2d0c8fc7f732653629e96e1450f1
Matthew Christopher <matthewlukechristopher@gmail.com>
2021-03-09 10:02:00 +0000

    Fix server crash on message deletion

--- src/server/custom-modules/chat/chat.js
+++ src/server/custom-modules/chat/chat.js
@@ -331,7 +331,7 @@ module.exports.initialise = (instance) => {
                     });
                   }
 
-                  if (firstResult[0] && fs.existsSync(path.join(__dirname, '../../../../user_files', firstResult[0].FileName ? firstResult[0].FileName : ''))) {
+                  if (firstResult[0].FileName && fs.existsSync(path.join(__dirname, '../../../../user_files', firstResult[0].FileName))) {
                     fs.unlink(path.join(__dirname, '../../../../user_files', firstResult[0].FileName), (error) => {
                       if (error) throw error;
 
